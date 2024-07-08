006aed111140483e50c5d34a98e40934126e9a21
chotabeck <mulenga99@gmail.com>
2024-07-08 16:50:20 +0200

    hey

--- Program.cs
+++ Program.cs
@@ -63,68 +63,68 @@ app.MapControllerRoute(
 
 app.MapRazorPages();
 
-using(var scope = app.Services.CreateScope())
-{
-    var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+// using(var scope = app.Services.CreateScope())
+// {
+//     var roleManager = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
 
-    var roles = new[] {"Admin", "OrderFul", "Stock", "OrderProcessing","Accounts","Shipping","Receiving","Sales"};
-    foreach(var role in roles)
-    {
-        if(!roleManager.RoleExistsAsync(role).Result)
-        {
-            roleManager.CreateAsync(new IdentityRole(role)).Wait();
-        }
-    }
-}
-using (var scope = app.Services.CreateScope())
-{
-    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+//     var roles = new[] {"Admin", "OrderFul", "Stock", "OrderProcessing","Accounts","Shipping","Receiving","Sales"};
+//     foreach(var role in roles)
+//     {
+//         if(!roleManager.RoleExistsAsync(role).Result)
+//         {
+//             roleManager.CreateAsync(new IdentityRole(role)).Wait();
+//         }
+//     }
+// }
+// using (var scope = app.Services.CreateScope())
+// {
+//     var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
     
 
-    var usersToCreate = new List<(string Email, string Password, string Role)>
-    {
-        ("admin@admin.com", "Admin@123.", "Admin"),
-        ("orderFul@OrderFul.com", "OrderFul@123.", "OrderFul"),
-        ("stock@stock.com", "Stock@123.", "Stock"),
-        ("orderprocessing@orderprocessing.com", "OrderProcessing@123.", "OrderProcessing"),
-        ("accounts@accounts.com", "Accounts@123.", "Accounts"),
-        ("shipping@shipping.com", "Shipping@123.", "Shipping"),
-        ("recieving@recieving.com", "Recieving@123.", "Recieving"),
-        ("sales@sales.com", "Sales@123.", "Sales")
+//     var usersToCreate = new List<(string Email, string Password, string Role)>
+//     {
+//         ("admin@admin.com", "Admin@123.", "Admin"),
+//         ("orderFul@OrderFul.com", "OrderFul@123.", "OrderFul"),
+//         ("stock@stock.com", "Stock@123.", "Stock"),
+//         ("orderprocessing@orderprocessing.com", "OrderProcessing@123.", "OrderProcessing"),
+//         ("accounts@accounts.com", "Accounts@123.", "Accounts"),
+//         ("shipping@shipping.com", "Shipping@123.", "Shipping"),
+//         ("recieving@recieving.com", "Recieving@123.", "Recieving"),
+//         ("sales@sales.com", "Sales@123.", "Sales")
     
-    };
+//     };
 
-    foreach (var (email, password, role) in usersToCreate)
-    {
-        try
-        {
-            if (await userManager.FindByEmailAsync(email) == null)
-            {
-                var user = new IdentityUser
-                {
-                    UserName = email,
-                    Email = email,
+//     foreach (var (email, password, role) in usersToCreate)
+//     {
+//         try
+//         {
+//             if (await userManager.FindByEmailAsync(email) == null)
+//             {
+//                 var user = new IdentityUser
+//                 {
+//                     UserName = email,
+//                     Email = email,
                    
-                };
-                var result = await userManager.CreateAsync(user, password);
-                if (result.Succeeded)
-                {
-                    await userManager.AddToRoleAsync(user, role);
-                }
-                else
-                {
-                    // Handle user creation failure
-                    Console.WriteLine($"Failed to create user {email}: {string.Join(", ", result.Errors.Select(e => e.Description))}");
-                }
-            }
-        }
-        catch (Exception ex)
-        {
-            // Handle exceptions
-            Console.WriteLine($"An error occurred while processing user {email}: {ex.Message}");
-        }
-    }
-}
+//                 };
+//                 var result = await userManager.CreateAsync(user, password);
+//                 if (result.Succeeded)
+//                 {
+//                     await userManager.AddToRoleAsync(user, role);
+//                 }
+//                 else
+//                 {
+//                     // Handle user creation failure
+//                     Console.WriteLine($"Failed to create user {email}: {string.Join(", ", result.Errors.Select(e => e.Description))}");
+//                 }
+//             }
+//         }
+//         catch (Exception ex)
+//         {
+//             // Handle exceptions
+//             Console.WriteLine($"An error occurred while processing user {email}: {ex.Message}");
+//         }
+//     }
+// }
 
 
 app.Run();
